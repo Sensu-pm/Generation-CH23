97259c3221bd12ec98ceb409c421ccd080a5b484
Jonathan-Sensu-pm <inme.jonathan.dominguez@gmail.com>
2023-02-17 17:46:55 -0600

    Terminamos de hacer la calculadora funcional y empezar a darle estilo

--- JS/js06/script.js
+++ JS/js06/script.js
@@ -89,9 +89,9 @@ document.body.append(imagenPerrito);
 //2do paso: Modificar el nodo (inner)
 
 var resultadoQueCambia= document.getElementById("resultado");
-resultadoQueCambia.innerHTML = "Saludos, cambio el resultado Juas Juas";
+resultadoQueCambia.innerHTML = "All we need is LOVE";
 
-
+//Construir calculadora
 
 var input1 = document.getElementById("input1");
 var input2 = document.getElementById("input2");
@@ -102,3 +102,81 @@ var botonMultiplicacion = document.getElementById("botonMultiplicacion");
 var botonDivision = document.getElementById("botonDivision");
 
 var resultado = document.getElementById("resultado");
+
+//Construir las funciones de la calculadora
+function suma(){
+    let valor1 = parseInt(input1.value);  //<input>
+    let valor2 = parseInt(input2.value);  //8
+    let suma = valor1 + valor2 ; //calculo la suma
+    resultado.innerHTML = suma;  //pongo el resultado en su lugar
+}
+function resta(){
+    let valor1 = parseInt(input1.value);  //<input>
+    let valor2 = parseInt(input2.value);  //8
+    let resta = valor1 - valor2 ; //calculo la suma
+    resultado.innerHTML = resta;  //pongo el resultado en su lugar
+
+}
+function multiplicacion(){
+    let valor1 = parseInt(input1.value);  //<input>
+    let valor2 = parseInt(input2.value);  //8
+    let multiplicacion = valor1 * valor2 ; //calculo la suma
+    resultado.innerHTML = multiplicacion;  //pongo el resultado en su lugar
+
+}
+function division(){
+    let valor1 = parseInt(input1.value);  //<input>
+    let valor2 = parseInt(input2.value);  //8
+    let division = valor1 / valor2 ; //calculo la suma
+    resultado.innerHTML = division;  //pongo el resultado en su lugar
+
+}
+
+/*Como se crea un evento (addEventlistener)
+        - node.addEventListener ("evento a escuchar", lo que quiero que haga cuando se escuche)
+
+*/
+botonSuma.addEventListener("click", suma);
+botonResta.addEventListener("click", resta);
+botonMultiplicacion.addEventListener("click", multiplicacion);
+botonDivision.addEventListener("click", division);
+
+
+/*
+
+Manipulacion del DOM
+
+
+    - Metodos para acceder a elementos 
+
+        - document.getElementById
+        - document.getElementsByTagName
+        - document.getElementosByClassName
+
+
+    - Metodos para crear elementos  
+
+        - document.createElement(etiqueta)
+        - document.createTextNode(texto) - Investigar
+
+
+    - Metodos para insertar elementos
+
+        - parentElement.append
+        - parentElement.insertBefore
+        - parentElement.insertAdjacentElement
+
+
+    - Metodos para modificar elementos
+
+        - node.outerHTML (leer o referenciar el elemento)
+        - node.innerHTML (modificar el elemento)
+
+
+*/
+
+const textoAModificar = document.querySelector("#h1")
+
+function cambiarColor(color){
+textoAModificar.style.color =color;
+}
