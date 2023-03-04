5914614e5d04c0c3fa6060a0cb474ada3f763a85
Jonathan-Sensu-pm <inme.jonathan.dominguez@gmail.com>
2023-03-04 00:02:54 -0600

    Collections

--- JAVA/java08/colecciones/src/colecciones/jada/MisCollections.java
+++ JAVA/java08/colecciones/src/colecciones/jada/MisCollections.java
@@ -1,10 +1,98 @@
 package colecciones.jada;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class MisCollections {
 
 	public static void main(String[] args) {
+		MisCollections c = new MisCollections();
 		
-		wrapperClass();
+		//imprimir(c.listaCollection());
+		//imprimir(c.setCollections());
+		//imprimir((Collection) c.mapCollections());
+		
+		mapCollections();
+		//wrapperClass();
+
+	}
+	
+	
+	public static void mapCollections() {
+		Map miMap = new HashMap();
+		miMap.put("valor1", "Juan");
+		miMap.put("valor2", "Maria");
+		miMap.put("valor3", "Arturo");
+		miMap.put("valor4", "Karla");
+		
+		imprimir(miMap.keySet());
+		imprimir(miMap.values());
+
+	}
+	
+	Set setCollections() {
+		Set miSet = new HashSet();
+		miSet.add("uno");
+		miSet.add("dos");		
+		miSet.add("tres");		
+		miSet.add("cuatro");
+		miSet.add("dos");
+	
+		
+		//Ejemplos personales de Set
+		miSet.remove("uno"); //debemos especificar cual queremos quitar
+		System.out.println("tamaño de la lista" + miSet.size());
+		
+		//miSet.clear();
+		
+		boolean em= miSet.isEmpty(); //dice true si esta vacio, debemos especificar el tipo que nos regresara
+		
+		System.out.println(em);
+		
+		Object a[]=  miSet.toArray();
+		for (int i = 0; i < a.length; i++) {
+			System.out.println("Este es un areglo"+ a[i]);
+		}
+		return miSet;
+	}
+	
+	
+	
+	private ArrayList listaCollection() {
+		ArrayList miLista = new ArrayList();
+		System.out.println(miLista+"tamaño de la lista "+ miLista.size());
+		System.out.println("esta vacia "+miLista.isEmpty());
+		
+		miLista.add("1");
+		miLista.add(0, 2);
+		miLista.add(3);
+		miLista.add(0, "patote");
+		
+		miLista.set(0, miLista);
+		miLista.remove(2);
+		
+		System.out.println(miLista+"tamaño de la lista "+ miLista.size());
+		System.out.println("esta vacia "+miLista.isEmpty());
+		
+		boolean e = miLista.contains(3);
+		System.out.println("---->"+ e);
+		
+		return miLista;
+
+	}
+	
+	
+	
+	private static void imprimir(Collection collection) {
+		
+		for (Object elementos : collection) {
+			System.out.println("elementos "+ elementos);
+		}
 
 	}
 	
