0ebff19d4878b63e99e958e42afb994e9f3e5b17
Jonathan-Sensu-pm <inme.jonathan.dominguez@gmail.com>
2023-03-06 23:36:25 -0600

    Tema de herencia y encapsulamiento

---
+++ JAVA/java09/oop/src/oop/clases/Fecha.java
@@ -0,0 +1,35 @@
+package oop.clases;
+
+public class Fecha(int dia, int mes, int año) {
+
+}
+
+public Fecha(int dia, int mes, int anio) {
+	super();
+	this.dia = dia;
+	this.mes = mes;
+	this.anio = anio;
+}
+	public String diaMesAño() {
+		return
+	}
+
+public int getDia() {
+	return dia;
+}
+public void setDia(int dia) {
+	this.dia = dia;
+}
+public int getMes() {
+	return mes;
+}
+public void setMes(int mes) {
+	this.mes = mes;
+}
+public int getAnio() {
+	return getAnio();
+}
+public void setAnio(int anio) {
+	this.mes = mes;
+}
+
