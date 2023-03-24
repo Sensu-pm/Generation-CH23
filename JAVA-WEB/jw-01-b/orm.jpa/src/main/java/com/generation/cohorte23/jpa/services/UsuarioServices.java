5134ed2679c433e5c08bf55326621665956831a6
Jonathan-Sensu-pm <inme.jonathan.dominguez@gmail.com>
2023-03-24 17:55:25 -0600

    Se enlazo con  Postman y utilizamos CRUD

--- JAVA-WEB/jw-01-b/orm.jpa/src/main/java/com/generation/cohorte23/jpa/services/UsuarioServices.java
+++ JAVA-WEB/jw-01-b/orm.jpa/src/main/java/com/generation/cohorte23/jpa/services/UsuarioServices.java
@@ -1,23 +1,46 @@
 package com.generation.cohorte23.jpa.services;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.generation.cohorte23.jpa.models.UsuarioModel;
 import com.generation.cohorte23.jpa.repositories.UsuarioRepositorio;
 
 @Service
-
 public class UsuarioServices {
 	@Autowired
-	UsuarioRepositorio usuarioRepositorio;
-	public ArrayList<UsuarioModel> busquedaPrioridad (Integer prioridad){
-		return usuarioRepositorio.busquedaPorPrioridad(prioridad);
+	UsuarioRepositorio usuariorepositorio;
+	@DeleteMapping
+	public boolean eliminarUsuario(Long id) {
+		try {
+			usuariorepositorio.deleteById(id);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
-	public ArrayList<UsuarioModel> listarUsuarios() {
-		return (ArrayList<UsuarioModel>)usuarioRepositorio.findAll();
+		
+		
+
+	public ArrayList<UsuarioModel> ListarUsuarios() {
+		return (ArrayList<UsuarioModel>) usuariorepositorio.findAll();
 
 	}
+	
+	public UsuarioModel guardarUsuario(UsuarioModel usuario) {
+		return usuariorepositorio.save(usuario);
+	}
+	
+	public ArrayList<UsuarioModel> obtenerPorPrioridad(Integer prioridad) {
+		return usuariorepositorio.findByPrioridad(prioridad);
+	}
+
+	public Optional<UsuarioModel> obtenerPorId(Long id){
+		return usuariorepositorio.findById(id);
+	}
+	
 }
